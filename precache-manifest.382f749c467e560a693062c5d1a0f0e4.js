50100c9bddc30b1405daf2677253143504818146
vladyslav223 <vladyslav223@gmail.com>
2019-09-15 20:33:49 +0300

    Updates

---
+++ precache-manifest.382f749c467e560a693062c5d1a0f0e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9f20909db7ad8c0b4eb3456d61952d",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "8ec523ddab68d51314fb",
+    "url": "/react_static-list-of-posts/static/css/main.948b31d0.chunk.css"
+  },
+  {
+    "revision": "fa67027c564020748778",
+    "url": "/react_static-list-of-posts/static/js/2.78963138.chunk.js"
+  },
+  {
+    "revision": "8ec523ddab68d51314fb",
+    "url": "/react_static-list-of-posts/static/js/main.d98edb40.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
